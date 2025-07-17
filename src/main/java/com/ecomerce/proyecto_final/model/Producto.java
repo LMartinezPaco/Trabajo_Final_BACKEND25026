99575a5b712d5ddb5f86bce2eceba79741bf55c1
Laura <lauramichellemartinez15@gmail.com>
2025-07-17 17:29:14 -0300

    First commit

---
+++ src/main/java/com/ecomerce/proyecto_final/model/Producto.java
@@ -0,0 +1,19 @@
+package com.ecomerce.proyecto_final.model;
+
+public class Producto {
+
+    private Long id;
+    private String nombre;
+    private double precio;
+
+    public Producto () {}
+
+    public Producto (Long id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Long getId() {return id;}
+    public String 
+}
